1b8f810cfb2ad0e8ceac3d409acea33d2c5dcac3
Jide Kareem <jidekrm@gmail.com>
2020-07-27 12:16:19 -0500

    Paircast - https://app.paircast.io/c/jHiVQ7jQc/4320058

--- MYIVSTestJay/app/src/main/java/com/example/myivstestv/MainActivity.java
+++ MYIVSTestJay/app/src/main/java/com/example/myivstestv/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.myivstestv;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.amazonaws.ivs.player.Cue;
 import com.amazonaws.ivs.player.Player;
+import com.amazonaws.ivs.player.PlayerException;
 import com.amazonaws.ivs.player.PlayerView;
+import com.amazonaws.ivs.player.Quality;
 import com.example.myivstestv.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +23,47 @@ public class MainActivity extends AppCompatActivity {
 
         PlayerView playerView = binding.playerView;
         Player player = playerView.getPlayer();
-        
+        player.addListener(new Player.Listener() {
+            @Override
+            public void onCue(@NonNull Cue cue) {
+                
+            }
+
+            @Override
+            public void onDurationChanged(long l) {
+
+            }
+
+            @Override
+            public void onStateChanged(@NonNull Player.State state) {
+
+            }
+
+            @Override
+            public void onError(@NonNull PlayerException e) {
+
+            }
+
+            @Override
+            public void onRebuffering() {
+
+            }
+
+            @Override
+            public void onSeekCompleted(long l) {
+
+            }
+
+            @Override
+            public void onVideoSizeChanged(int i, int i1) {
+
+            }
+
+            @Override
+            public void onQualityChanged(@NonNull Quality quality) {
+
+            }
+        });
 
     }
 }
