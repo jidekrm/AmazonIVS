e680c80cea6021edb0aa2a176c335d71dcd6272b
Jide Kareem <jidekrm@gmail.com>
2020-07-27 12:14:38 -0500

    Paircast - https://app.paircast.io/c/jHiVQ7jQc/4218611

--- MYIVSTestJay/app/src/main/java/com/example/myivstestv/MainActivity.java
+++ MYIVSTestJay/app/src/main/java/com/example/myivstestv/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.myivstestv.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding =  ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        
+
     }
 }
